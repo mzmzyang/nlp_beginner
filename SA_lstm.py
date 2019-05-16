2b6933e5e2bcd2f803acd3c9dd42a7f336f89332
mzmzyang <liyang.ly@hotmail.com>
2019-05-16 09:58:33 +0800

    modify embedding size and get public score 0.63236

--- SA_lstm.py
+++ SA_lstm.py
@@ -7,9 +7,10 @@ import torch.utils.data as Data
 import torch.functional as F
 
 
-def vocab_list(train_data, test_data):
+def vocab_list(train_data, vali_data, test_data):
     word_list = []
     train_words = []
+    vali_words = []
     test_words = []
     for phrase in train_data:
         t = []
@@ -17,21 +18,23 @@ def vocab_list(train_data, test_data):
             word_list.append(word.lower())
             t.append(word.lower())
         train_words.append(t)
-
-    for phrase in test_data:
+    for phrase in vali_data:
         t = []
         for word in phrase.split(' '):
             word_list.append(word.lower())
             t.append(word.lower())
+        vali_words.append(t)
+    for phrase in test_data:
+        t = []
+        for word in phrase.split(' '):
+            t.append(word.lower())
         test_words.append(t)
-    return list(set(word_list)), train_words, test_words
+    return list(set(word_list)), train_words, vali_words, test_words
 
 
 def transition(word_list):
     word2idx = {word: idx + 1 for idx, word in enumerate(word_list)}
     idx2word = {idx + 1: word for idx, word in enumerate(word_list)}
-    word2idx['<unk>'] = 0
-    idx2word[0] = '<unk>'
     return word2idx, idx2word
 
 
@@ -63,24 +66,26 @@ def pad_phrase(encode_data, max_length):
 
 def precess_dataset(max_length):
     train_dataset = pd.read_csv("./dataset/train.tsv", sep='\t')
+    test_dataset = pd.read_csv("./dataset/test.tsv", sep='\t')
     # print(train_dataset.info())
     train_phrase = train_dataset['Phrase']
+    test_phrase = test_dataset['Phrase']
     train_y = train_dataset['Sentiment'].values
-    # train_y = train_y.reshape(-1, 1)
 
-    test_phrase = train_phrase[120000:]
+    vali_phrase = train_phrase[120000:]
     train_phrase = train_phrase[:120000]
-    test_y = train_y[120000:]
+    vali_y = train_y[120000:]
     train_y = train_y[:120000]
 
-    word_list, train_words, test_words = vocab_list(train_phrase, test_phrase)
-    word_size = len(word_list)
+    word_list, train_words, vali_words, test_words = vocab_list(train_phrase, vali_phrase, test_phrase)
+    word_size = len(word_list) + 1
     word2idx, idx2word = transition(word_list)
 
     train_x = pad_phrase(encode_phrase(train_words, word2idx), max_length)
+    vali_x = pad_phrase(encode_phrase(vali_words, word2idx), max_length)
     test_x = pad_phrase(encode_phrase(test_words, word2idx), max_length)
 
-    return word_size + 1, word2idx, idx2word, train_x, test_x, train_y, test_y
+    return word_size + 1, word2idx, idx2word, train_x, vali_x, train_y, vali_y, test_x
 
 
 class MySA(nn.Module):
@@ -103,19 +108,20 @@ class MySA(nn.Module):
 
 
 LR = 0.01
-EPOCH = 50
+EPOCH = 5
 MAX_LENGTH = 25
 BATCH_SIZE = 128
 
-vocb_size, word2idx, idx2word, train_x, test_x, train_y, test_y = precess_dataset(MAX_LENGTH)
+vocb_size, word2idx, idx2word, train_x, vali_x, train_y, vali_y, test_x = precess_dataset(MAX_LENGTH)
 train_x = torch.LongTensor(train_x)
 train_y = torch.tensor(train_y)
+vali_x = torch.LongTensor(vali_x)
+vali_y = torch.tensor(vali_y)
 test_x = torch.LongTensor(test_x)
-test_y = torch.tensor(test_y)
 train_set = Data.TensorDataset(train_x, train_y)
 train_loader = Data.DataLoader(dataset=train_set, batch_size=BATCH_SIZE, shuffle=True)
 
-mySA = MySA(vocb_size, 50, 50, 1, 5)
+mySA = MySA(vocb_size, 20, 20, 1, 5)
 print(mySA)
 optimizer = torch.optim.Adam(mySA.parameters(), lr=LR)
 loss_func = nn.CrossEntropyLoss()
@@ -134,10 +140,25 @@ for epoch in range(EPOCH):
 
         if idx % 100 == 0:
             mySA.eval()
-            test_x = test_x.cuda()
-            test_y = test_y.cuda()
-            test_output = mySA(test_x)
-            pred_output = torch.max(test_output, dim=1)[1]
-            accuracy = float(torch.sum(pred_output == test_y)) / float(test_y.size(0))
-            print('Epoch: ', epoch, '| train loss: %.4f' % loss.item(), '| test accuracy: %.4f' % accuracy)
+            vali_x = vali_x.cuda()
+            vali_y = vali_y.cuda()
+            vali_output = mySA(vali_x)
+            pred_output = torch.max(vali_output, dim=1)[1]
+            accuracy = float(torch.sum(pred_output == vali_y)) / float(vali_y.size(0))
+            print('Epoch: ', epoch, '| train loss: %.4f' % loss.item(), '| validation accuracy: %.4f' % accuracy)
             mySA.train()
+
+mySA.eval()
+test_x = test_x.cuda()
+pred_test = mySA(test_x)
+pred_output = torch.max(pred_test, dim=1)[1]
+pred_output = pred_output.type(torch.int32)
+
+sub_file = pd.read_csv("./dataset/test.tsv", sep='\t')
+print(sub_file.info())
+del sub_file['SentenceId']
+del sub_file['Phrase']
+print(sub_file.info())
+sub_file['Sentiment'] = pred_output.cpu()
+sub_file.to_csv("./dataset/submission.csv", index=False)
+print(sub_file.info())
